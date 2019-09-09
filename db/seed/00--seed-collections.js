96ffa3f6afcfb83c73a3958ec066619b385480cf
Hammad Malik <tomatohammado@gmail.com>
2019-09-09 12:25:16 -0400

    Notes about seeding the DB
    - The Kingdom model seeds just fine!

--- db/seed/00--seed-collections.js
+++ db/seed/00--seed-collections.js
@@ -1,13 +1,34 @@
+/** Import Models */
 const Monarch = require("../../models/Monarch");
+const Kingdom = require("../../models/Kingdom");
+/** Import JSON raw seed data */
 const monarchRawData = require("../data/monarchRaw.json");
+const kingdomRawData = require("../data/kingdomRaw.json");
 
-const monarchSeedData = monarchRawData.map(monarchRaw => {
+/** Map through monarchRawData to create an array of objects that match the basic fields in the Monarch model.
+ *
+ * We are excluding the "kingdom" field for now because it requires an ObjectId, and we need to seed the collections first.
+ *
+ * We'll address this in the `01--seed-related-models` file
+ */
+
+const monarchSeedData = monarchRawData.map(monarchRawItem => {
   const monarchSeedItem = {};
-  monarchSeedItem.name = monarchRaw.name;
-  monarchSeedItem.house = monarchRaw.house;
-  monarchSeedItem.start = monarchRaw.start;
-  monarchSeedItem.end = monarchRaw.end;
-  monarchSeedItem.endReason = monarchRaw.endReason;
+
+  monarchSeedItem.name = monarchRawItem.name;
+  monarchSeedItem.house = monarchRawItem.house;
+  monarchSeedItem.start = monarchRawItem.start;
+  monarchSeedItem.end = monarchRawItem.end;
+  monarchSeedItem.endReason = monarchRawItem.endReason;
 
   return monarchSeedItem;
+});
+
+/** We can seed the Kingdom data directly from the raw JSON.
+ *
+ * Mongoose will disregard the extra fields in the JSON when creating the documents.
+ */
+
+Kingdom.deleteMany({}).then(() => {
+  Kingdom.create(kingdomRawData).then(kingdoms => console.log(kingdoms));
 });
