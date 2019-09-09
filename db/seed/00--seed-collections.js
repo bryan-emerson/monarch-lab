01bb446c68a039d49aa6a379f1d73b8298637343
Hammad Malik <tomatohammado@gmail.com>
2019-09-09 12:15:59 -0400

    Demonstrate mapping through raw Monarch data

--- db/seed/00--seed-collections.js
+++ db/seed/00--seed-collections.js
@@ -1,6 +1,13 @@
 const Monarch = require("../../models/Monarch");
 const monarchRawData = require("../data/monarchRaw.json");
 
-Monarch.deleteMany({}).then(() => {
-  Monarch.create(monarchRawData).then(monarchs => console.log(monarchs));
+const monarchSeedData = monarchRawData.map(monarchRaw => {
+  const monarchSeedItem = {};
+  monarchSeedItem.name = monarchRaw.name;
+  monarchSeedItem.house = monarchRaw.house;
+  monarchSeedItem.start = monarchRaw.start;
+  monarchSeedItem.end = monarchRaw.end;
+  monarchSeedItem.endReason = monarchRaw.endReason;
+
+  return monarchSeedItem;
 });
