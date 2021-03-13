f69faea1da343e4ab44bdf5fca852525f2270854
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-13 15:27:33 -0500

    00 kingdom seed instantiated

--- db/seed/00--seed-collections.js
+++ db/seed/00--seed-collections.js
@@ -6,16 +6,27 @@ let kingdomModel = ('../models/Kingdom.js')
 let kingdomJson = require('./data/kingdomRaw.json')
 
 let destructureMonarch =
-  ({name, house, start, end, endReason, kingdom})=>
-  ({name, capital, region, population})
+  ({ name, house, start, end, endReason, kingdom }) =>
+    ({ name, capital, region, population })
 
 
-  let destructuredMonarchs = (monarchJson.map(destructureMonarch))
+let destructuredMonarchs = (monarchJson.map(destructureMonarch))
 
-  monarchModel
-      .deleteMany({})
-      .then(()=> monarchModel.create(destructuredMonarchs))
-      .then(mongoose.disconnect)
-      .then(()=> console.log('done!'))
+monarchModel
+  .deleteMany({})
+  .then(() => monarchModel.create(destructuredMonarchs))
+  .then(mongoose.disconnect)
+  .then(() => console.log('done!'))
 
 
+let destructureKingdom =
+  ({ title, extract }) =>
+    ({ title, extract })
+
+let destructuredKingdom = (kingdomJson.map(destructureKingdom))
+
+kingdomModel
+  .deleteMany({})
+  .then(() => kingdomModel.create(destructuredKingdom))
+  .then(mongoose.disconnect)
+  .then(()=>console.log('done!'))
